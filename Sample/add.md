6e90a45a51d35c6904df25567a06a45f3f5fe6c6
Shibaken28 <8128kanzensu@gmail.com>
2023-11-13 11:25:31 +0900

    feat: add sample

---
+++ Sample/add.md
@@ -0,0 +1,11 @@
+---
+title: Sample / 足し算のサンプル
+documentation_of: ./add.hpp
+---
+
+## 概要
+足し算をします
+
+## 使い方
+`sum(a,b)`で$a+b$を計算します
+
---
+++ Sample/add.test.cpp
@@ -0,0 +1,10 @@
+# define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+# include <iostream>
+# include "add.hpp"
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << sum(a, b) << endl;
+}
